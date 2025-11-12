ccb63cea7f78fda6742b651b529bff37ee04eee3
MOXHATKA <MOXHATKA@protonmail.com>
2025-11-12 22:25:15 +0300

    Получение полей у типов

--- src/main.go
+++ src/main.go
@@ -7,6 +7,8 @@ import (
 	"unicode"
 )
 
+var index int
+
 type Type struct {
 	name        string
 	description string
@@ -24,7 +26,7 @@ type Method struct {
 	name        string
 	description string
 	note        string
-	parameter   []Parameter
+	parameters  []Parameter
 }
 
 type Parameter struct {
@@ -44,58 +46,87 @@ func main() {
 	// var result string
 
 	for {
-		tagH4Open := "<h4>"
-		tagH4Close := "</h4>"
-
-		indexH4Start := strings.Index(dataString, tagH4Open)
-		if indexH4Start == -1 {
+		// получение <h4> ... </h4>
+		var tagString string
+		index, tagString = getInnerData(dataString, "<h4>", "</h4>")
+		if index == -1 {
 			break
 		}
 
-		indexEndOffsetByStart := strings.Index(dataString[indexH4Start:], tagH4Close)
-		indexH4End := indexH4Start + indexEndOffsetByStart + len(tagH4Close)
-
-		tagString := dataString[indexH4Start:indexH4End]
+		indexH4End := index
 
 		// получение имени
-		indexNameStart := strings.Index(tagString, "</a>") + 4
-		indexNameEnd := strings.Index(tagString, "</h4>")
-		name := tagString[indexNameStart:indexNameEnd]
-
+		var name string
+		index, name = getInnerData(tagString, "</a>", "")
 		fmt.Println("Название: " + name)
 
 		// получение описания
-		indexDescStart := strings.Index(dataString, "<p>") + 3
-		indexDescEndOffestByStart := strings.Index(dataString[indexDescStart:], "</p>")
-		indexDescEnd := indexDescStart + indexDescEndOffestByStart
-		desc := dataString[indexDescStart:indexDescEnd]
+		var desc string
+		index, desc = getInnerData(dataString, "<p>", "</p>")
 		fmt.Println("Описание: " + desc + "\n")
 
+		// проверка на наличие таблицы
 		nextH4 := strings.Index(dataString[indexH4End:], "<h4>")
 		nextTbody := strings.Index(dataString[indexH4End:], "<tbody>")
-
 		if nextH4 != -1 && nextTbody > nextH4 {
 			dataString = dataString[nextH4:]
 			continue
 		}
 
-		indexTbodyStart := strings.Index(dataString, "<tbody>") + 7
-		indexTbodyOffsetByStart := strings.Index(dataString[indexTbodyStart:], "</tbody>")
-		indexTbodyEnd := indexTbodyStart + indexTbodyOffsetByStart
-		fields := dataString[indexTbodyStart:indexTbodyEnd]
-
-		fmt.Println("Поля: " + fields)
-
 		// получение полей
+		var fields string
+		index, fields = getInnerData(dataString, "<tbody>", "</tbody>")
+		// fmt.Println("Поля: " + fields)
+
+		// создание структур Type | Method
 		if unicode.IsUpper(rune(name[0])) {
-			typeTG := &Type{}
+			typeTG := Type{}
 			typeTG.name = name
+			typeTG.description = desc
+
+			for {
+				var i int
+
+				var name string
+				i, name = getInnerData(fields, "<td>", "</td>")
+				if i == -1 {
+					break
+				}
+				fields = fields[i:]
+
+				var typeField string
+				i, typeField = getInnerData(fields, "<td>", "</td>")
+
+				if i == -1 {
+					break
+				}
+				fields = fields[i:]
+
+				var desc string
+				i, desc = getInnerData(fields, "<td>", "</td>")
+
+				if i == -1 {
+					break
+				}
+
+				field := Field{
+					name:        name,
+					typeField:   typeField,
+					description: desc,
+				}
+
+				fmt.Print("field: ", field.name, ", ")
+				fmt.Print("type: ", field.typeField, ", ")
+				fmt.Print("desc: ", field.description, "\n")
+				fields = fields[i:]
+			}
+
 		} else {
-			methodTG := &Method{}
+			methodTG := Method{}
 			methodTG.name = name
 		}
 
-		dataString = dataString[indexTbodyEnd:]
+		dataString = dataString[index:]
 	}
 
 	// f, err := os.Create("./output/result.html")
@@ -107,4 +138,20 @@ func main() {
 	// if err != nil {
 	// 	panic("Не получилось записать в файл")
 	// }
+}
+
+func getInnerData(dataString, tagStart, tagEnd string) (int, string) {
+	indexStart := strings.Index(dataString, tagStart) + len(tagStart)
+	if indexStart-len(tagStart) == -1 {
+		return -1, ""
+	}
+	var indexEnd int
+	if tagEnd == "" {
+		indexEnd = len(dataString)
+	} else {
+		indexOffset := strings.Index(dataString[indexStart:], tagEnd)
+		indexEnd = indexStart + indexOffset
+	}
+
+	return indexEnd, dataString[indexStart:indexEnd]
 }
